347da339329ee7fda712dfa9a7d44c4f939826a9
ojha5858 <47297222+ojha5858@users.noreply.github.com>
2025-12-18 15:52:32 +0000

    hhhh

--- backend/src/config/googleAuth.js
+++ backend/src/config/googleAuth.js
@@ -1,5 +1,8 @@
 const { google } = require("googleapis");
 
+if (!process.env.GOOGLE_SERVICE_ACCOUNT_JSON) {
+  throw new Error("GOOGLE_SERVICE_ACCOUNT_JSON is missing");
+}
 const auth = new google.auth.GoogleAuth({
   keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
   scopes: ["https://www.googleapis.com/auth/indexing"],
