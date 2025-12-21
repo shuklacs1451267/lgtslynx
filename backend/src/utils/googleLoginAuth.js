128027fda678fb4aad096a85560e63c63d803aad
ojha5858 <shubhamojha2898@gmail.com>
2025-12-22 04:18:55 +0530

    Revert "gggg"
    This reverts commit 5ca28c07634d6c1ca296cfbb3ac36ad3f677ffd2.

--- backend/src/utils/googleLoginAuth.js
+++ backend/src/utils/googleLoginAuth.js
@@ -5,7 +5,7 @@ const User = require("../models/User");
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://lgtslynx-production.up.railway.app/auth/google/callback",
+    callbackURL: "https://frontend-production-4e2f.up.railway.app/auth/google/callback",
     proxy: true,
 },
     async (accessToken, refreshToken, profile, done) => {
